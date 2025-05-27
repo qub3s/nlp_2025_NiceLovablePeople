a196a7a43c36de506fba417a3c152d826bb0fad9
Luca Francis <lucafrancis@web.de>
2025-05-27 02:40:47 +0200

    Install correct numpy version and add gpu sanity check (#3)
    * Install correct numpy version and add gpu sanity check
    
    * remove eol char
    
    * Update run_gpu_sanity_check.sh
    
    ---------
    
    Co-authored-by: GOESTERN-0994917 <u16865@glogin10.usr.hpc.gwdg.de>
    Co-authored-by: Niklas Bauer <nikbau2000@gmail.com>

--- run_gpu_sanity_check.sh
+++ run_gpu_sanity_check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=train-multitask_classifier
-#SBATCH -t 00:20:00                  # estimated time # TODO: adapt to your needs
+#SBATCH --job-name=gpu_sanity_check
+#SBATCH -t 00:00:10                  # estimated time # TODO: adapt to your needs
 #SBATCH -p grete:shared              # the partition you are training on (i.e., which nodes), for nodes see sinfo -p grete:shared --format=%N,%G
-#SBATCH -G A100:1                    # take 1 GPU, see https://docs.hpc.gwdg.de/compute_partitions/gpu_partitions/index.html for more options
+#SBATCH -G A100:1                    # take 1 GPU, see https://docs.hpc.gwdg.de/how_to_use/compute_partitions/gpu_partitions/index.html for more options
 #SBATCH --mem-per-gpu=8G             # setting the right constraints for the splitted gpu partitions
 #SBATCH --nodes=1                    # total number of nodes
 #SBATCH --ntasks=1                   # total number of tasks
@@ -31,4 +31,5 @@ echo "Latest Commit: $(git rev-parse --short HEAD)"
 echo -e "Uncommitted Changes: $(git status --porcelain | wc -l)\n"
 
 # Run the script:
-python -u multitask_classifier.py --use_gpu --local_files_only --option finetune --task sst --hidden_dropout_prob 0.1
+python -c "import torch;  num_of_gpus = torch.cuda.device_count(); print('Number of GPU:',num_of_gpus);"
+
--- setup_gwdg.sh
+++ setup_gwdg.sh
@@ -46,7 +46,7 @@ conda activate dnlp
 echo $CONDA_DEFAULT_ENV
 
 # Install packages
-conda install -y pytorch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 pytorch-cuda=12.1 -c pytorch -c nvidia
+conda install -y "numpy<2" pytorch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 pytorch-cuda=12.1 -c pytorch -c nvidia
 conda install -y tqdm==4.66.2 requests==2.31.0 transformers==4.38.2 tensorboard==2.16.2 tokenizers==0.15.1 -c conda-forge -c huggingface
 pip install explainaboard-client==0.1.4 sacrebleu==2.4.0
 
