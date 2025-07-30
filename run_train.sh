7d7a484e149d5be08db4cb24e6b06d005043481c
Dacasil <173367017+Dacasil@users.noreply.github.com>
2025-07-30 14:39:40 +0200

    change run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=run_all
-#SBATCH -t 00:40:00                  # estimated time # TODO: adapt to your needs
+#SBATCH -t 02:00:00                  # estimated time # TODO: adapt to your needs
 #SBATCH -p grete:shared              # the partition you are training on (i.e., which nodes), for nodes see sinfo -p grete:shared --format=%N,%G
 #SBATCH -G A100:1                    # take 1 GPU, see https://docs.hpc.gwdg.de/compute_partitions/gpu_partitions/index.html for more options
 #SBATCH --mem-per-gpu=8G             # setting the right constraints for the splitted gpu partitions
@@ -33,6 +33,18 @@ echo -e "Uncommitted Changes: $(git status --porcelain | wc -l)\n"
 
 # Run the script:
 
-# STS
+# ETPC
 echo -e "\nStarting ETPC\n"
 python -u multitask_classifier.py --use_gpu --option finetune --task etpc --epochs=50 --lr 2e-5
+
+# SST
+echo -e "\nStarting SST\n"
+python -u multitask_classifier.py --use_gpu --option finetune --task sst --hidden_dropout_prob 0.25 --epochs=6
+
+# STS
+echo -e "\nStarting STS\n"
+python -u multitask_classifier.py --use_gpu --option finetune --task sts --hidden_dropout_prob 0.25 --epochs=10
+
+# QQP
+echo -e "\nStarting QQP\n"
+python multitask_classifier.py --use_gpu --option finetune --task qqp --hidden_dropout_prob 0.1 --epochs=1
