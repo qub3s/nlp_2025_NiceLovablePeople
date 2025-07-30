2ee68d278f3428427f772da07021f7c209dda8f3
GOESTERN-1006111 <u17476@glogin10.usr.hpc.gwdg.de>
2025-07-30 21:52:38 +0200

    change run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -33,25 +33,25 @@ echo -e "Uncommitted Changes: $(git status --porcelain | wc -l)\n"
 
 # Run the script:
 # SST
-echo -e "\nStarting SST\n"
-python -u multitask_classifier.py --use_gpu --option finetune --task sst --hidden_dropout_prob 0.25 --epochs=6
+#echo -e "\nStarting SST\n"
+#python -u multitask_classifier.py --use_gpu --option finetune --task sst --hidden_dropout_prob 0.25 --epochs=6
 
 # STS
-echo -e "\nStarting STS\n"
-python -u multitask_classifier.py --use_gpu --option finetune --task sts --hidden_dropout_prob 0.25 --epochs=10
+#echo -e "\nStarting STS\n"
+#python -u multitask_classifier.py --use_gpu --option finetune --task sts --hidden_dropout_prob 0.25 --epochs=10
 
 # QQP
-echo -e "\nStarting QQP\n"
-python multitask_classifier.py --use_gpu --option finetune --task qqp --hidden_dropout_prob 0.1 --epochs=1
+#echo -e "\nStarting QQP\n"
+#python multitask_classifier.py --use_gpu --option finetune --task qqp --hidden_dropout_prob 0.1 --epochs=1
 
 # PTD-Bert
 echo -e "\nStarting PTD-Bert\n"
-python -u multitask_classifier.py --use_gpu --option finetune --task etpc --hidden_dropout_prob 0.25 --epochs=20 --lr 1e-6 --batch_size 16
+python -u multitask_classifier.py --use_gpu --option finetune --task etpc --hidden_dropout_prob 0.25 --epochs=10 --lr 1e-6 --batch_size 16
 
 # Paraphrase Type Detection
-echo -e "\nStarting PTD\n"
-python bart_detection.py --use_gpu
+#echo -e "\nStarting PTD\n"
+#python bart_detection.py --use_gpu
 
 # Paraphrase Generation
-echo -e "\nStarting PG\n"
-python -u bart_generation.py --use_gpu
+#echo -e "\nStarting PG\n"
+#python -u bart_generation.py --use_gpu
