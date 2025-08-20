b1889db8476d85dc8a9281284a3e4dd15102c045
Amon Pönitzsch </>
2025-08-20 18:19:16 +0200

    test-changes v2.1.1

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -483,7 +483,7 @@ def train_multitask(args):
                 b_labels = b_labels.to(device)
 
                 optimizer.zero_grad()
-                logits = model.predict_similarity(b_ids_1, b_mask_1, b_ids_2, b_mask_2)
+                logits = model.predict_paraphrase(b_ids_1, b_mask_1, b_ids_2, b_mask_2)
                 loss = F.binary_cross_entropy_with_logits(logits, b_labels.float())
 
                 if config.option == "finetune":
