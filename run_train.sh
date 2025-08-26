1b618bd4e182d45a6855de098c48b7f42bcf9c09
hamzasiddiqui10 <hamzaahmedsiddiqui10@gmail.com>
2025-08-26 16:31:58 +0200

    final dropout removed. lr increased, epochs increased

--- run_train.sh
+++ run_train.sh
@@ -34,8 +34,9 @@ echo -e "Uncommitted Changes: $(git status --porcelain | wc -l)\n"
 # Run the script:
 # SST
 echo -e "\nStarting SST\n"
-python -u multitask_classifier.py --use_gpu --option finetune --task sst --hidden_dropout_prob 0.20 --epochs 6 --batch_size 64 --lr 1e-5
-echo -e "\n SWM basic implementation with pos_score and neg_score features added on top of BERT's CLS embedding."
+echo -e "\n ---------->> SWM basic implementation with pos_score and neg_score features added on top of BERT's CLS embedding."
+python -u multitask_classifier.py --use_gpu --option finetune --task sst --hidden_dropout_prob 0.30 --epochs 10 --batch_size 64 --lr 1e-3
+echo -e "\n ---------->> SWM basic implementation with pos_score and neg_score features added on top of BERT's CLS embedding. /n no drop out layer on final CLS->5 layer"
 
 
 # STS
