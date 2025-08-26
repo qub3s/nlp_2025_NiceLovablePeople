b8abddb55862443a254bcc9de91cd6fc4b2d53b6
hamzasiddiqui10 <hamzaahmedsiddiqui10@gmail.com>
2025-08-26 17:09:08 +0200

    allah malik

--- run_train.sh
+++ run_train.sh
@@ -34,7 +34,7 @@ echo -e "Uncommitted Changes: $(git status --porcelain | wc -l)\n"
 # Run the script:
 # SST
 echo -e "\nStarting SST\n"
-python -u multitask_classifier.py --use_gpu --option finetune --task sst --hidden_dropout_prob 0.25 --epochs 10 --batch_size 64 --lr 1e-6
+python -u multitask_classifier.py --use_gpu --option finetune --task sst --hidden_dropout_prob 0.25 --epochs 20 --batch_size 64 --lr 1e-6
 echo -e "\n ---------->> SWM basic implementation with pos_score and neg_score features added on top of BERT's \n CLS embedding."
 
 
